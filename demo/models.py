423fcd708683efba31514dbbc13f6ee6d1749e50
Gaoyan Wang <gaoyanw68@gmail.com>
2022-12-06 16:32:17 -0800

    add field in models

--- demo/models.py
+++ demo/models.py
@@ -4,4 +4,9 @@ from django.db import models
 # create a model, a class represent a table in database
 class Book(models.Model):
     # a field will be a column in database
-    title = models.CharField(max_length=36)
+    title = models.CharField(max_length=36, blank=True, unique=True)
+    description = models.TextField(max_length=256, blank=True)
+    price = models.DecimalField(default=0, max_digits = 2, decimal_places=2)
+    is_published = models.BooleanField(default=False)
+    published = models.DateField(blank=True,  null=True, default=None)
+    cover = models.FileField(upload_to='covers/', blank=True)
