69bf8d594f6b57efffc2e03221d9b07dc279298c
Gaoyan Wang <gaoyanw68@gmail.com>
2022-12-06 22:59:32 -0800

    20.2 add bookAmin, display desired components

--- demo/admin.py
+++ demo/admin.py
@@ -4,4 +4,11 @@ from django.contrib import admin
 from django.contrib import admin
 from .models import Book
 # Register your models here.
-admin.site.register(Book)
+# admin.site.register(Book)
+# this is will register all books, not flexible to change
+# the following is a flexible way
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['title', 'description', 'price']
+    list_filter = ['published', 'price']
+    search_fields = ['title', 'description']
