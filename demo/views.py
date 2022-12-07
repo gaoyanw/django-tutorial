41a6632471620ef4f18ea6df900b74c33cc14d15
Gaoyan Wang <gaoyanw68@gmail.com>
2022-12-06 17:09:33 -0800

    use model object to query db # 17

--- demo/views.py
+++ demo/views.py
@@ -3,9 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from django.views import View
+from .models import Book
 def first(request):
     return HttpResponse("frist message from views, hello world")
 
+# access db by Book.objects.,  can be all, filter or get
 class Another(View):
+    books = Book.objects.all()
+    output = ""
+    for book in books:
+        output += f"we have {book.title} book in DB with ID {book.id}<br>"
     def get(self, request):
-        return HttpResponse("This is another function inside class, hello world")
+        return HttpResponse(self.output)
