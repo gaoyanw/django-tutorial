42ed9256f273f34b8f93c1c22024f4d30aef7149
Gaoyan Wang <gaoyanw68@gmail.com>
2022-12-06 23:31:33 -0800

    22. Serializers

--- demo/views.py
+++ demo/views.py
@@ -1,20 +1,6 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.http import HttpResponse
-from django.views import View
 from .models import Book
-from django.shortcuts import  render
-def first(request):
-    books = Book.objects.all()
-    # the books can be used in templates
-    return render(request, 'first_template.html', {'books': books})
-
-# access db by Book.objects.,  can be all, filter or get
-class Another(View):
-    books = Book.objects.all()
-    output = ""
-    for book in books:
-        output += f"we have {book.title} book in DB with ID {book.id}<br>"
-    def get(self, request):
-        return HttpResponse(self.output)
+from rest_framework import viewsets
+from .serializers import  BookSerializer
+class BookViewSet(viewsets.ModelViewSet):
+    serializer_class = BookSerializer
+    queryset = Book.objects.all()
